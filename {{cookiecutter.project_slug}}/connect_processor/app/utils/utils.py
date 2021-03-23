95c6059655ad841237aa6eea5d23dbc9d7d1a537
Neha Shelke <Neha.Shelke@ingrammicro.com>
2021-03-23 06:31:36 +0530

    unit tests for TierConfiguration and DynamicValidation 2

--- {{cookiecutter.project_slug}}/connect_processor/app/utils/utils.py
+++ {{cookiecutter.project_slug}}/connect_processor/app/utils/utils.py
@@ -9,36 +9,11 @@ class Utils:
             config = json.load(file_handle)
         return config
 
-    # @staticmethod
-    # def serialize(data, restrict=False):
-    #     """Convert given object to printable structure
-    #     Args:
-    #         request: Generic object to be converted in string with json structure
-    #         :param data: request to be converted
-    #         :param restrict: hide sensitive request
-    #     """
-    #     dumped = json.dumps(data, default=lambda o: getattr(o, '__dict__', str(o)), check_circular=False)
-    #     return Utils.drop_restricted_data(json.loads(dumped)) if restrict else json.loads(dumped)
-
-    # @staticmethod
-    # def drop_restricted_data(param):
-    #     params = deepcopy(param)
-    #     if params and isinstance(params, dict):
-    #         for param_key in params.keys():
-    #             if str(param_key).upper() in Globals.RESTRICTED_KEYS:
-    #                 params[param_key] = "####-###-####"
-    #             elif params[param_key] and isinstance(params[param_key], dict):
-    #                 params[param_key] = Utils.drop_restricted_data(params[param_key])
-    #
-    #     return params
 
     @staticmethod
     def is_null_or_empty(value):
         return value is None or value == 0 or len(value) == 0
 
-    # @staticmethod
-    # def get_status_code(info):
-    #     return info['statusCode'] if 'statusCode' in info else ''
 
     @staticmethod
     def get_item_by_id(items, item_id) -> object:
@@ -46,24 +21,6 @@ class Utils:
             if item['id'] == item_id:
                 return item
         raise Exception('Item {id} not found.'.format(id=item_id))
-
-    # @staticmethod
-    # def is_downsize_request(items):
-    #     for item in items:
-    #         if item.old_quantity > item.quantity:
-    #             return True
-    #
-    #     return False
-
-    # @staticmethod
-    # def check_previous_active_subscription(filter):
-    #     config_file = Utils.get_config_file()
-    #     fulfilment_resource = FulfillmentResource(Config(
-    #         api_url=config_file['apiEndpoint'],
-    #         api_key=config_file['apiKey'],
-    #         products=config_file['products']
-    #     ))
-    #     return fulfilment_resource.search_asset_request(filter)
 
 
 def get_basic_value(base, value):
@@ -76,13 +33,4 @@ def get_value(base, prop, value):
         return get_basic_value(base[prop], value)
     return '-'
 
-# def get_param_by_id(param_id):
-#     """ Get a parameter of the asset.
-#             :param str param_id: Id of the the parameter to get.
-#             :return: The parameter with the given id, or ``None`` if it was not found.
-#             :rtype: :py:class:`.Param` | None
-#             """
-#     try:
-#         return list(filter(lambda param: param.id == param_id, params))[0]
-#     except IndexError:
-#         return None
+
---
+++ {{cookiecutter.project_slug}}/tests/test_tier_fulfillment.py
@@ -0,0 +1,30 @@
+import unittest
+
+from cnct import ConnectClient
+from connect_processor.app.tier_fulfillment import TierConfiguration
+from unittest.mock import patch, MagicMock
+from test_util import TestUtils
+
+config_file = TestUtils.get_config_file()
+# apiEndpoint is the API End-point of Connect
+connect_api_url = config_file['connectApiEndpoint']
+# apiKey is the API key for authorization created in Integrations menu of Connect
+connect_key = config_file['connectApiKey']
+# products are the list of IDs of the products which needs to be processed by this Processor
+# client = ConnectClient(api_key=connect_key[0], endpoint=connect_api_url[0])
+client = ''
+
+class TestTierConfiguration(unittest.TestCase):
+    # //////////////////////
+    # TIER CONFIGURATION UNIT TESTS
+    # /////////////////////
+
+    @patch('connect_processor.app.tier_fulfillment.TierConfiguration.update_parameters',
+           MagicMock(return_value=""))
+    @patch('connect_processor.app.tier_fulfillment.TierConfiguration.approve_request',
+            MagicMock(return_value=TestUtils.get_response("purchase_subscription_response.json")))
+    def test_purchase_pass(self):
+        request = TestUtils.get_response("create_purchase_request_body.json")
+        response = TestUtils.get_response("purchase_subscription_response.json")
+        result = TierConfiguration.process_request(request, client)
+        self.assertDictEqual(result, response)
