a6b44cd54e17bda13a507e35dd70a10b1649b1d1
Neha Shelke <Neha.Shelke@ingrammicro.com>
2021-03-25 01:31:26 +0530

    final dynamic validation

--- {{cookiecutter.project_slug}}/connect_processor/app/dynamic_validation.py
+++ {{cookiecutter.project_slug}}/connect_processor/app/dynamic_validation.py
@@ -1,12 +1,20 @@
+
 from flask import Flask, request, json
 
 api = Flask(__name__)
 
+"""
+# TO execute on local
+set FLASK_APP=service
+flask run
+"""
+
 def get_parameter_by_id(params, id):
     for param in params:
-         if param['id'] == id:
-               return param
+        if param['id'] == id:
+            return param
     raise Exception('Parameter {id} not found.'.format(id=id))
+
 
 def set_parameter(params, param):
     ret = []
@@ -17,28 +25,40 @@ def set_parameter(params, param):
             ret.append(p)
     return ret
 
-def get_parameters(data):
-    data = data['asset']['params']
-    return data
+
+def get_validation_request_data(request):
+    data = request.data.decode("utf-8")
+    jsondata = json.loads(data)
+    return jsondata
+
 
 # The webhook cofigured in Connect will call the validate method to validate the value provided as ordering parameter during order placement
-@api.route('/validate', methods=['POST'])
+@api.route('/validate', methods=['POST', 'GET'])
 def do_validate():
 
-        data = request.return_value
-        params = get_parameters(data)
-        # Customize: Change the parameter ID as per configured in Connect Product. Here param ID is 'param_dynamic_validation'
-        param_1 = get_parameter_by_id(params, 'param_dynamic_validation')
-        # If the validation fails, fill in the error message in value_error param
-        # Customize: Implement validation logic and add desired error message
-        # param_1['value_error'] = 'This error is from the validation script!'
+    jsondata = get_validation_request_data(request)
+    params = jsondata['asset']['params']
+    # Customize: replace 'param_dynamic_validation' with Id of the parameter that requires to be validated
+    param_1 = get_parameter_by_id(params, 'param_dynamic_validation')
+    # Customize: Implement the desired logic to validate the value provided as the parameter
+    if param_1 and param_1['value'].isnumeric():
+        return api.response_class(
+            response=json.dumps(jsondata),
+            status=200,
+            mimetype='application/json'
+        )
+
+    else:
+        # Customize: Provide proper error message
+        param_1['value_error'] = "This error is from the validation script! Value should be numeric."
         params = set_parameter(params, param_1)
+        jsondata['asset']['params'] = params
+        response = json.dumps(jsondata)
+        return api.response_class(
+            response=response,
+            status=400,
+            mimetype='application/json'
+        )
 
-        # Customize: Ensure the parameter id in addressed here matches with the parameter id configured in the Product in Vendor Portal
-        # Add more validations as required. And customize the validation logic and error message
-
-        data['asset']['params'] = params
-        # Returning the response with the 'value_error' param filled will fail the dynamic validation and display error message on the ordering screen
-        return data
 
 
--- {{cookiecutter.project_slug}}/tests/test_dynamic_validation.py
+++ {{cookiecutter.project_slug}}/tests/test_dynamic_validation.py
@@ -19,11 +19,11 @@ class TestDynamicValidation(unittest.TestCase):
     # DYNAMIC VALIDATION UNIT TESTS
     # /////////////////////
 
-    @patch('connect_processor.app.dynamic_validation.request',
+    @patch('connect_processor.app.dynamic_validation.get_validation_request_data',
            MagicMock(return_value=TestUtils.get_response("dynamic_validation_request.json")))
 
     def test_dynamic_validation_pass(self):
 
         response = TestUtils.get_response("dynamic_validation_request.json")
         result = do_validate()
-        self.assertDictEqual(result, response)
+        self.assertDictEqual(result.get_json(), response)
